2eda22c4f0924d8ac134420bd96fd0d7578c2657
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2015-06-14 00:05:02 +0000

    adding server

---
+++ jsx/Index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react');
+
+var Index = React.createClass({
+    render: function () {
+        return (<div>hello</div>);
+    }
+});
+
+module.exports = Index;
