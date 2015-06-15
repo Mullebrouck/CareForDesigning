dcbcf00b14a085b4296d14f9f1b4a6fd1f96563f
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2015-06-15 04:51:25 +0000

    Connecting form to Google

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+
+var request = require('superagent');
+
+var postUrl = 'https://docs.google.com/forms/d/1h0yHX8FR_M_9YxfAOosdfwZXt8W-5PueZoBHuTQp0cI/formResponse';
 
 var app = express();
 /*eslint-disable new-cap */
@@ -14,10 +19,36 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/', router);
 
 router.get('/', function(req, res) {
     res.render('index', { title: 'Kaatje Mullebrouck' });
+});
+
+router.post('/contact', function(req, res) {
+    var form = {
+        'entry.1187367439': req.body.form.name,
+        'entry.2042372520': req.body.form.email,
+        'entry.1292810695': req.body.form.message
+    };
+
+    request.post(postUrl)
+        .type('form')
+        .accept('application/json')
+        .send(form)
+        .end(function(err, response) {
+            if(err) {
+                console.log('err:', err);
+            } else {
+                console.log('success google form', JSON.stringify(response.body));
+            }
+        });
+
+    res.send({
+        res: { name: form['entry.1187367439']}
+    });
 });
 
 // catch 404 and forward to error handler
--- jsx/pages/Contact.js
+++ jsx/pages/Contact.js
@@ -3,8 +3,17 @@
 var React = require('react');
 var Island = require('../components/Island');
 var Footer = require('../components/Footer');
+var Utils = require('../utils/ContactUtils');
 
 var Contact = React.createClass({
+    getInitialState: function () {
+        return {
+            name: '',
+            email: '',
+            message: '',
+            isSent: false
+        };
+    },
     render: function () {
         return (
             <div className="contact">
@@ -13,15 +22,29 @@ var Contact = React.createClass({
                   <p className="contact-information">
                     San Jose, CA 95128 kaatje.mullebrouck@gmail.com
                   </p>
-                  <div className="contact-form">
-                    <input className="contact-name" type="text"
-                      placeholder="Name"/>
-                    <input className="contact-email" type="email"
-                      placeholder="Name@email.com"/>
-                    <textarea className="contact-message"
-                      placeholder="Message" rows="10"/>
-                    <div className="contact-submit"> Submit </div>
-                  </div>
+                    {this.state.isSent
+                        ? (<h2 className="contact-thanks">Thank you!</h2>)
+                        : (
+                   <form className="contact-form" onSubmit={this.onSubmit}>
+                        <input className="contact-name" type="text"
+                              value={this.state.name}
+                               name="name"
+                               autocomplete="name"
+                              onChange={this.onChangeName}
+                              placeholder="Name"/>
+                        <input className="contact-email" type="email"
+                              value={this.state.email}
+                               name="email"
+                               autocomplete="email"
+                              onChange={this.onChangeEmail}
+                              placeholder="Name@email.com"/>
+                        <textarea className="contact-message"
+                              value={this.state.message}
+                              onChange={this.onChangeMessage}
+                              placeholder="Message" rows="10"/>
+                        <button className="contact-submit" type="submit"> Submit </button>
+                      </form>
+                    )}
 
                   <div className="contact-socialMedia">
                     <a href="https://twitter.com/kaatjemulle"
@@ -45,6 +68,28 @@ var Contact = React.createClass({
                 <Footer />
             </div>
         );
+    },
+    onChangeName: function (e) {
+        this.setState({
+            name: e.target.value
+        });
+    },
+    onChangeEmail: function (e) {
+        this.setState({
+            email: e.target.value
+        });
+    },
+    onChangeMessage: function (e) {
+        this.setState({
+            message: e.target.value
+        });
+    },
+    onSubmit: function (e) {
+        e.preventDefault();
+        Utils.post(this.state);
+        this.setState({
+            isSent: true
+        });
     }
 });
 
