9686b7a0e081c96bf8ac6d84e4780cd148c313a8
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2015-06-15 09:12:23 +0000

    Connecting gridItems and its nav.
    Populates all grid items from a dictionary.
    Activates the navbar in GridHeader.
    Populates navbar in GridHeader with the categories from the CategoriesData.js

---
+++ jsx/data/GridItemsData.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  'wedding': {
+    image: 'images/WeddingNailArt_Edgy.png',
+    name: 'Magazine',
+    slides: [
+      {
+        image: 'images/WeddingNailArt_Edgy.png',
+        title: 'Magazine',
+        credits: 'Mockup'
+      }
+    ]
+  },
+  'atSea': {
+    image: 'images/WeddingNailArt_Edgy.png',
+    name: 'atSea',
+    slides: [
+      {
+        image: 'images/WeddingNailArt_Edgy.png',
+        title: 'atSea 1',
+        credits: 'Mockup'
+      }
+    ]
+  }
+};
