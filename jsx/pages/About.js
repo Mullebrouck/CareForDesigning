450d6e7786c82908060bcc1854230528934d1b53
Kaatje Mullebrouck <kaatje.mullebrouck@gmail.com>
2015-06-21 04:25:59 +0000

    Updated Artist Statement

--- jsx/pages/About.js
+++ jsx/pages/About.js
@@ -10,15 +10,17 @@ var About = React.createClass({
             <div className="about">
                 {this.props.children}
                 <Island title={"About"}>
-                  <p className="about-information"> Lorem ipsum dolor sit amet,
-                  consectetur adipiscing elit,
-                  sed do eiusmod tempor incididunt ut labore et dolore magna
-                  aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                  ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  Duis aute irure dolor in reprehenderit in voluptate velit
-                  esse cillum dolore eu fugiat nulla pariatur.
-                  Excepteur sint occaecat cupidatat non proident,
-                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  <p className="about-information">
+                  Since I was a kid I was passionate and driven to expres
+                  my creativity in art and music.
+                  <br />
+                  That is why I decided to make the career switch
+                  to Graphic Design. Now I feel like I can express and explore
+                  my freedom in a more effective and convincing way.
+                  I love thinking outside the box and I am very eager to learn.
+                  <br />
+                  In my spare time I enjoy learning new software languages
+                  like HTML & SCSS, Java, React and Javascript.
                   </p>
                   <a className="about-button" target="_blank"
                     href="/downloads/Resume Kaatje Mullebrouck.pdf"> Resume </a>
