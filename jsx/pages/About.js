9c18200404921a6979cc664e28e1fe4a809d53b3
Jorge Cuadra <jorgecuadrafueyo@gmail.com>
2015-06-21 06:56:53 +0000

    about page fixes

--- jsx/pages/About.js
+++ jsx/pages/About.js
@@ -11,21 +11,26 @@ var About = React.createClass({
                 {this.props.children}
                 <Island title={"About"}>
                   <p className="about-information">
-                  Since I was a kid, I was passionate and driven to expres
-                  my creativity in art and music.
-                  <br />
-                  That is why I decided to make the career switch
-                  to Graphic Design. Now I feel like I can expres and explore
-                  my freedom in a more effective and convincing way.
-                  I love thinking outside the box and I am very eager to learn.
-                  <br />
-                  In my spare time I enjoy learning new software languages
-                  like HTML, SCSS, Java, ReactJS and Javascript.
+                      {'Since I was a kid, I was passionate and driven to' +
+                      ' express my creativity in art and music.'}
+                  </p>
+                  <p className="about-information">
+                      {'That is why I decided to make the career switch ' +
+                      'to Graphic Design. Now I feel like I can express ' +
+                      'and explore my freedom in a more effective and ' +
+                      'convincing way. I love thinking outside the box ' +
+                      'and I am very eager to learn.'}
+                  </p>
+                  <p className="about-information">
+                      {'In my spare time I enjoy learning new software ' +
+                      'languages like HTML, SCSS, Java, ReactJS and Javascript.'}
                   </p>
                   <a className="about-button" target="_blank"
                     href="/downloads/Kaatje Mullebrouck Resume.pdf"> Resume </a>
                   <a className="portfolio-button" target="_blank"
-                      href="/downloads/Kaatje Mullebrouck Portfolio.compressed.pdf"> Portfolio </a>
+                      href="/downloads/Kaatje Mullebrouck Portfolio.compressed.pdf">
+                      Portfolio
+                  </a>
                 </Island>
                 <Footer />
             </div>
