1054e4b0549519d9640db90c0d0d76031ffd0094
Kaatje Mullebrouck <kaatje.mullebrouck@gmail.com>
2015-06-21 02:25:42 +0000

    Adjust slides

--- jsx/data/GridItemsData.js
+++ jsx/data/GridItemsData.js
@@ -4,12 +4,27 @@ module.exports = {
   wedding: {
     image: 'images/WeddingNailArt_Edgy-Thumbnail.png',
     name: 'Nail Art',
-    description: ['TODO description'],
+    description: [
+      'This magazine project contained three different styles:',
+      'formal, edgy and playful.',
+      'I created the layout and design of the magazine articles.',
+      'Credits for text and images go to bridalmusing.com'
+    ],
     slides: [
       {
+        image: 'images/WeddingNailArt_Playfull.png',
+        title: 'Playful Layout for Wedding Nail Art',
+        credits: 'Text and Images: bridalmusings.com'
+      },
+      {
         image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        title: 'Edgy Layout for Wedding Nail Art',
+        credits: 'Text and Images: bridalmusings.com'
+      },
+      {
+        image: 'images/WeddingNailArt_ Formal.png',
+        title: 'Formal Layout for Wedding Nail Art',
+        credits: 'Text and Images: bridalmusings.com'
       }
     ]
   },
@@ -17,14 +32,66 @@ module.exports = {
   atSea: {
     image: 'images/@Sea_logo_thumbnail.jpg',
     name: '@Sea',
-    description: ['TODO description'],
+    description: [
+      'Designing and producing a real-world graphic design campaign ' +
+      'for a seafood restaurant. ',
+      'Design Brief, Logo Design, Branding and Identity, Packaging, ' +
+      'Advertisement, Website Layout, Presentation and Setting up ' +
+      'Files for Printing.',
+      'The full Campaign Presentation is available at the bottom of this page'
+      ],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'atSea 1',
-        credits: 'Mockup',
-        buttonLink: '/downloads/',
-        buttonText: 'Campaign presentation'
+        image: 'images/@Sea_logo.jpg',
+        title: 'Logo Design @Sea',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_stationary.jpg',
+        title: 'BusinessCards, Letter and Enveloppe',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_menu_main.jpg',
+        title: 'Menu Closed and Open Both Sides',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_menu_open.jpg',
+        title: 'Menu One Side Open',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_advertisement_magazine.jpeg',
+        title: 'Advertisement Page Art Revenue',
+        credits: ''
+      },
+      {
+        image: 'images/@Sea_Newspaper.jpg',
+        title: 'Newspaper Announcement ',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_package_mockup.jpg',
+        title: '@Sea Wine Packaging ',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_labels.jpg',
+        title: 'Sea Wine Labels Front and Back',
+        credits: ' '
+      },
+      {
+        image: 'images/@sea_web_third.jpg',
+        title: 'Restaurant Website Page ',
+        credits: ' '
+      },
+      {
+        image: 'images/@Sea_web_6.jpg',
+        title: 'Lunch Website Page ',
+        credits: ' ',
+        buttonLink: '/downloads/Campaign Presentation (72).pdf',
+        buttonText: 'Campaign Presentation'
       }
     ]
   },
@@ -33,21 +100,31 @@ module.exports = {
     image: 'images/Automobuild_thumbnail.jpg',
     name: 'Automobuild',
     description: [
-      'Automobuild is a toy-store located downtown Los Gatos. ',
+      'Automobuild is a toy-store located downtown Los Gatos, California. ',
       'I designed and formatted text and graphics ' +
-        'for blog posts, newsletters, fliers, advertisement and ' +
-        'brochures.'
+      'for blog posts on social media, newsletters, flyers, advertisement ' +
+      'brochure, and the automobuild.com website.'
     ],
     slides: [
       {
-        image: 'images/Automobuild_Flyer_HB.jpg',
-        title: 'Automobuild Flyer Happy birds Event',
-        credits: 'Mock Up : SparkleStock'
+        image: 'images/Automobuild_Brochure.jpg',
+        title: 'Tri-Fold Information Brochure ',
+        credits: 'Mockup: Mike Adam Wood'
+      },
+      {
+        image: 'images/Automobuild_Flyer_CC.jpg',
+        title: 'Flyer for Calico Critter Day 2013 and 2014',
+        credits: 'Mockup : SparkleStock'
       },
       {
         image: 'images/Automobuild_Flyer_HB.jpg',
-        title: 'Automobuild Flyer Happy birds Event',
-        credits: 'Mock Up : SparkleStock'
+        title: 'Flyer for Happy Birds Event  2013',
+        credits: 'Mockup : SparkleStock'
+      },
+      {
+        image: 'images/Automobuild_Coupon.jpg',
+        title: '20% off coupon',
+        credits: ''
       }
     ]
   },
@@ -55,12 +132,17 @@ module.exports = {
   businessCards: {
     image: 'images/Jorge_thumbnail.jpeg',
     name: 'Businesscards',
-    description: ['TODO description'],
+    description: [''],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        image: 'images/Jorge_businesscard.jpg',
+        title: 'Business Card Jorge Cuadra, Software Engineer',
+        credits: 'Mock Up: GraphicBurger'
+      },
+      {
+        image: 'images/SweetBelly_businesscard.jpg',
+        title: 'Business Cards Special Candy Store, Sweet Belly',
+        credits: 'Mockup: GraphicBurger'
       }
     ]
   },
@@ -68,12 +150,17 @@ module.exports = {
   mvla: {
     image: 'images/MVLA_thumbnaili.jpg',
     name: 'MVLA',
-    description: ['TODO description'],
+    description: ['T-shirt design for soccer club MVLA, California in 2014. '],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        image: 'images/MVLA_Roundi.jpg',
+        title: 'MVLA T-shirt design round',
+        credits: 'Mockup: TKoenings design agency & DSGner'
+      },
+      {
+        image: 'images/MVLA_rectangle.jpg',
+        title: 'MVLA T-shirt design square',
+        credits: 'Mockup: TKoenings design agency & DSGner'
       }
     ]
   },
@@ -81,13 +168,43 @@ module.exports = {
   milow: {
     image: 'images/Milow_CD_front_thumbnail.jpg',
     name: 'Milow',
-    description: ['TODO description'],
+    description: [
+      'Milow is a singer, songwriter living in Belgium.',
+      'For one of my projects I designed the Label, cover and booklet for ' +
+      'his cd “the bigger picture”.',
+      'The full booklet pdf is available at the bottom of this page.'
+      ],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup',
-        buttonLink: '/downloads/',
+        image: 'images/Milow_CD_Front.jpg',
+        title: 'Milow Front view',
+        credits: 'Mockup: Pixeden'
+      },
+      {
+        image: 'images/Milow_CD-Cover_back.jpg',
+        title: 'Milow Back View',
+        credits: 'Mockup: Pixeden'
+      },
+      {
+        image: 'images/cd-pack.jpg',
+        title: 'Full Printable Front and Back Cover and Inside',
+        credits: ''
+      },
+      {
+        image: 'images/Milow_Booklet_FP.jpg',
+        title: 'Booklet Milow',
+        credits: 'Mockup: Pixeden'
+      },
+      {
+        image: 'images/Milow_Booklet.jpg',
+        title: 'Booklet Milow Middle',
+        credits: 'Mockup: Pixeden'
+      },
+      {
+        image: 'images/Milow_Booklet_LP.jpg',
+        title: 'Booklet Milow',
+        credits: 'Mockup: Pixeden',
+        buttonLink: '/downloads/booklet milow.pdf',
         buttonText: 'Booklet'
       }
     ]
@@ -96,12 +213,12 @@ module.exports = {
   whiteRice: {
     image: 'images/Package_rice_thumbnail.jpg',
     name: 'White Rice Packaging',
-    description: ['TODO description'],
+    description: ['Redesigning of the Minute White Rice packaging. '],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        image: 'images/Package_rice.jpg',
+        title: 'Minute White Rice Packaging',
+        credits: 'Mockup: GraphicsFuel'
       }
     ]
   },
@@ -109,12 +226,22 @@ module.exports = {
   sweetBelly: {
     image: 'images/SweetBelly_Stationary_Thumbnail.jpg',
     name: 'Sweet Belly',
-    description: ['TODO description'],
+    description: [
+      'Sweet Belly is a special candy store where ' +
+      'you can order and personalize chocolate ' +
+      'beans for special occasions like birth, ' +
+      'communion, or wedding.'
+      ],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        image: 'images/SweetBelly_businesscard.jpg',
+        title: 'Business Cards for Special Candy Store, Sweet Belly',
+        credits: 'Mockup: GraphicBurger'
+      },
+      {
+        image: 'images/SweetBelly_Stationary.jpg',
+        title: 'Collateral for Sweet Belly',
+        credits: ''
       }
     ]
   },
@@ -122,12 +249,20 @@ module.exports = {
   articles: {
     image: 'images/Magazine_article_thumbnail.jpg',
     name: 'Articles',
-    description: ['TODO description'],
+    description: [
+      'For this magazine project I created the ' +
+      'overall layout and the text.'
+    ],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        image: 'images/Magazine_Article_2.png',
+        title: 'Splash Battle Article',
+        credits: ''
+      },
+      {
+        image: 'images/Magazine_article.jpg',
+        title: 'Friendship Article',
+        credits: ''
       }
     ]
   },
@@ -135,12 +270,27 @@ module.exports = {
   posters: {
     image: 'images/Poster_projects_Thumbnail.png',
     name: 'Poster Projects',
-    description: ['TODO description'],
+    description: [''],
     slides: [
       {
-        image: 'images/WeddingNailArt_Edgy.png',
-        title: 'Magazine',
-        credits: 'Mockup'
+        image: 'images/Poster_ThinkGreen.jpg',
+        title: 'Think Green Poster',
+        credits: ''
+      },
+      {
+        image: 'images/Poster_Poo_1.jpg',
+        title: 'Phantom of The Opera Layout 1',
+        credits: 'I did not design the chandelier.'
+      },
+      {
+        image: 'images/Poster_Poo_2.jpg',
+        title: 'Phantom of The Opera Layout 2',
+        credits: ''
+      },
+      {
+        image: 'images/Poster_Poo_3.jpg',
+        title: 'Phantom of The Opera Layout 3',
+        credits: ''
       }
     ]
   }
